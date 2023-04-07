8c2fa86caf42ff005f0830cd24b33a35e45a9d70
sanghunlee <simoniful0@gmail.com>
2023-04-07 11:30:14 +0900

    Feat: Add Stack struct

---
+++ CompilerTutorial/CompilerTutorial/Playground/CompilerTutorial.playground/Sources/Stack.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct Stack {
+  private var list = [String]()
+  
+  func isEmpty() -> Bool {
+    return self.list.isEmpty
+  }
+  
+  func count() -> Int {
+    return self.list.count
+  }
+  
+  func top() -> String? {
+    return self.list.last
+  }
+  
+  mutating func push(item: String) {
+    self.list.append(item)
+  }
+  
+  mutating func pop() {
+    self.list.popLast()
+  }
+}
