e435e386806cf9890183020107b287afacd20a0b
sanghunlee <simoniful0@gmail.com>
2023-04-07 11:34:59 +0900

    Feat: Add Valiator class

---
+++ CompilerTutorial/CompilerTutorial/Playground/CompilerTutorial.playground/Sources/Validator.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+class Validator {
+  enum ArrayError: Error {
+    case invalidValue
+  }
+  
+  func isArray(str: String) throws -> Bool {
+    
+    var stack: Stack = Stack()
+    
+    for element in str {
+      if !stack.isEmpty() && stack.top() == "[" && element == "]" {
+        stack.pop()
+      } else if element == "]" || element == "[" {
+        stack.push(item: String(element))
+      }
+    }
+    
+    if stack.count() == 0 {
+      return true
+    } else {
+      throw ArrayError.invalidValue
+    }
+  }
+  func isSeperator(letter: Character) -> Bool {
+    return letter == "[" || letter == "]" || letter == ","
+  }
+  func isQuote(letter: Character) -> Bool {
+    return letter == "\'" || letter == "\""
+  }
+  func isString(quoteArray: [Character]) -> Bool {
+    return quoteArray.count == 0
+  }
+}
